3af21c1fb6b891fb3af1714e753a76adb3749481
Keichi Takahashi <keichi.t@me.com>
2013-12-11 19:00:33 +0900

    Added netns shell launch command

--- launch.sh
+++ launch.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-sudo killall tlldpd
 sudo ip netns exec host1 ../tiny-lldpd/tlldpd -d
 sudo ip netns exec host2 ../tiny-lldpd/tlldpd -d
 sudo ip netns exec host3 ../tiny-lldpd/tlldpd -d
 sudo ip netns exec host4 ../tiny-lldpd/tlldpd -d
+
+trema netns host1
+
+sudo killall tlldpd
